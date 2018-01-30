0ae07085d0f8d7703885a1087635efa9153e3de2
Aaron Bray <aaron.bray@kitware.com>
2018-01-29 21:02:51 -0500

    Pull latest KWANT with CMake updates

--- CMake/add_project_kwant.cmake
+++ CMake/add_project_kwant.cmake
@@ -9,7 +9,7 @@ ExternalProject_Add(kwant
   DEPENDS ${KWANT_DEPENDENCIES}
   PREFIX ${DIVA_BINARY_DIR}
   GIT_REPOSITORY "git://github.com/Kitware/kwant.git"
-  GIT_TAG 646e08fb7a664eb8d7f26439de6f66a99af57ae2
+  GIT_TAG 48555efdda860e81b44a1f7a07bcdbbfb6ef244f
   SOURCE_DIR kwant
   BINARY_DIR kwant-build
   STAMP_DIR ${DIVA_STAMP_DIR}
