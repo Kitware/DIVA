58e6ebe7456748f63d790d9ec32f2eb6501b4e57
as6520 <ameya.shringi@kitware.com>
2018-11-12 03:06:12 -0500

    starter scripts for the algorithms

--- pipelines/tmux_act_aod.sh
+++ pipelines/tmux_act_aod.sh
@@ -5,26 +5,28 @@
 # 
 # Usage:
 #
-#   ./tmux_rc3d.sh  <environment_setup_script> <rc3d_home> <vis_directory> <algorithm_time>
+#   ./tmux_act.sh  <environment_setup_script> <act_home> <darknet_root> <vis_directory> <algorithm_time>
 #
 # environment_setup_script: A script (possibly setup_DIVA.sh) that
 # sets the enviroment up properly so that pipeline_runner will be found in
 # PATH and will execute properly
 #
-# rc3d_home: path to the home directory of RC3D 
+# act_home: path to the home directory of act 
+#
+# darknet_home: path to the home directory of darknet
 #
 # vis_directory: directory where visualizations are dumped
 #
 # algorithm_time: approx time taken by the pipeline to output visualization
 #-
 
-SESSION="rc3d_demo"
-
+SESSION="act_aod_demo"
 
 START_SCRIPT=$1
-RC3D_HOME=$2
-VIS_DIRECTORY=$3
-ALGORITH_TIME=$4
+ACT_HOME=$2
+DARKNET_HOME=$3
+VIS_DIRECTORY=$4
+ALGORITH_TIME=$5
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #cd ${SCRIPT_DIR}
@@ -37,7 +39,7 @@ echo "$(date) Starting Image Sender..."
 tmux select-window -t $SESSION:0
 tmux rename-window -t $SESSION:0 'Sender0'
 tmux send-keys -t $SESSION:0 "source ${START_SCRIPT}" C-m
-tmux send-keys -t $SESSION:0 "pipeline_runner --pipe ${SCRIPT_DIR}/image_sender.pipe --set exp:experiment_file_name=etc/input_stream.yml --set zmq_timg:port 5560" C-m
+tmux send-keys -t $SESSION:0 "pipeline_runner --pipe ${SCRIPT_DIR}/image_sender.pipe --set exp:experiment_file_name=etc/input_stream.yml --set zmq_timg:port 5760" C-m
 
 sleep 1
 tmux split-window -h -t $SESSION:0
@@ -45,15 +47,15 @@ tmux select-pane -t 0
 tmux split-window -t $SESSION:0
 
 sleep 1
-echo "$(date) Starting RC3D Instance..."
-tmux send-keys -t 1 "export PYTHONPATH=${RC3D_HOME}/experiments/virat" C-m
+echo "$(date) Starting ACT Instance..."
+tmux send-keys -t 1 "export PYTHONPATH=${ACT_HOME}/act-detector-scripts:${ACT_HOME}/virat-act-detector-scripts:${ACT_HOME}/python:${PYTHONPATH}" C-m
 tmux send-keys -t 1 "source ${START_SCRIPT}" C-m
+tmux send-keys -t 1 "source ${ACT_HOME}/act-detector/bin/activate" C-m
 tmux send-keys -t 1 "mkdir -p ${VIS_DIRECTORY}" C-m
-tmux send-keys -t 1 "pipeline_runner --pipe ${SCRIPT_DIR}/rc3d.pipe --set rc3d:experiment_file_name=${RC3D_HOME}/experiments/virat/experiment.yml --set rc3d:model_cfg=${RC3D_HOME}/experiments/virat/td_cnn_end2end.yml --set zmq::port=5560 --set img_writer::file_name_template=${VIS_DIRECTORY}/image%06d.jpg" C-m
+tmux send-keys -t 1 "pipeline_runner --pipe ${SCRIPT_DIR}/act_aod_receiver.pipe --set act_process:exp=${ACT_HOME}/virat-act-detectory-scripts/rgb_eval_1b.yml --set --set zmq::port=5760 --set img_writer::file_name_template=${VIS_DIRECTORY}/image%06d.jpg" C-m
 
 sleep 1
 echo "$(date) Starting display..."
 tmux send-keys -t 2 "dsplay.sh ${VIS_DIRECTORY} ${ALGORITHM_TIME}" C-m
-
 
 echo "$(date) Starter script done!"
