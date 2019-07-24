de19766964651e29700ee702d13a6fd437107ec2
Ameya Shringi <ameya.shringi@kitware.com>
2019-07-24 18:34:07 -0400

    Remove image list and yml files for a chunk too

--- scripts/cli_helpers/cleanup_chunk.py
+++ scripts/cli_helpers/cleanup_chunk.py
@@ -3,6 +3,13 @@ import os
 import warnings
 import json
 
+def remove_file(file_path):
+    if os.path.exists(file_path):
+        print("Removing {0}".format(file_path))
+        os.remove(file_path)
+    else:
+        warnings.warn("{0} not found".format(file_path))
+
 def main(args):
     if not os.path.exists(args.chunk_json):
         raise OSError("Invalid path {0} provided for Chunk.json".format(args.chunk_json))
@@ -10,13 +17,17 @@ def main(args):
     if args.chunk_id in chunks.keys():
         video_names = chunks[args.chunk_id]["files"]
         for video_name in video_names:
-            video_file = "{0}.json".format(os.path.splitext(video_name)[0])
-            video_path = os.path.join(args.cache_dir, video_file)
-            if os.path.exists(video_path):
-                print("Removing {0}".format(video_path))
-                os.remove(video_path)
-            else:
-                warnings.warn("Json file associated with {0} not found in {1}".format( video_name, args.cache_dir))
+            video_name = os.path.splitext(video_name)[0]
+            json_file = "{0}.json".format(video_name)
+            yml_file = "{0}_experiment.yml".format(video_name)
+            imagelist_file = "{0}_imagelist.txt".format(video_name)
+            json_path = os.path.join(args.cache_dir, json_file)
+            yml_path  =  os.path.join(args.cache_dir, yml_file)
+            imagelist_path = os.path.join(args.cache_dir, imagelist_file)
+            remove_file(json_path)
+            remove_file(yml_path)
+            if os.path.exists(imagelist_path):
+                remove_file(imagelist_path)
     else:
         raise KeyError("Invalid chunk id {0} provided".format(args.chunk_id))
 
