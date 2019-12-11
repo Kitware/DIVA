c6ca86da5405180bead89a2e70f9928bcde87c1e
David Joy <david.joy@krs-3004.khq.kitware.com>
2019-12-11 16:33:00 -0500

    Fixed registered processes path for Python wheel

--- python/diva/processes/register_cpp_processes.py
+++ python/diva/processes/register_cpp_processes.py
@@ -5,4 +5,4 @@ import diva
 
 def get_cpp_path():
     module_path = os.path.dirname(os.path.abspath(diva.__file__))
-    return os.path.join(module_path, "processes")
+    return os.path.join(module_path, "lib", "processes")
