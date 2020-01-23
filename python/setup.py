5317c0aa01c99fc9c10ef9a301190acf2dee17c3
dmjoy <10147749+dmjoy@users.noreply.github.com>
2020-01-22 19:03:37 -0500

    Updated DIVA wheel version for new Kwiver wheel (1.4.5) (#52)
    * Updated DIVA wheel version for new Kwiver wheel (1.4.5)

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ with open('../README.rst') as f:
 
 diva_source_dir = ".."
 setup(name='diva-framework',
-      version='0.0.5',
+      version='0.0.6',
       author='Kitware, Inc.',
       author_email='diva-framework@kitware.com',
       url='https://github.com/Kitware/DIVA',
@@ -22,7 +22,7 @@ setup(name='diva-framework',
           'scikit-build'
       ],
       install_requires=[
-          'kwiver>=1.4.2',
+          'kwiver==1.4.5',
           'opencv-python'
       ],
       cmake_args=[
